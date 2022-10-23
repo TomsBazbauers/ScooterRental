e432dc9f560331e181c755e741c675ac1b82cace
tms <tms.bzbrs@gmail.com>
2022-10-23 15:25:52 +0300

    init

---
+++ ScooterRental.Core/Validations/ScooterPropertyValidations/ScooterValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScooterRental.Core.Validations
+{
+    public class ScooterValidator : IScooterValidator
+    {
+        public bool IsValid(string id, decimal pricePerMinute)
+        {
+            return !string.IsNullOrEmpty(id) && pricePerMinute > 0;
+        }
+    }
+}
