90860f978189cf0ab6319c21bb5767fc90e6feac
tms <tms.bzbrs@gmail.com>
2022-10-26 14:00:29 +0300

    init

--- ScooterRental/Models/IncomeReportRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScooterRental.Models
-{
-    public class IncomeReportRequest
-    {
-        public int? Year { get; set; }
-
-        public bool IncludeRunningRentals { get; set; }
-    }
-}
